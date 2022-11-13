9cbaa4cdfeeb89f12b3348727ecfcb831e96fc90
Ekaterina Kovergina <ekaterina.kovergina@idp.zyfra.com>
2022-11-14 01:10:54 +0300

    fix tests

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,11 +5,11 @@ import Input from './input';
 import Error from './error';
 import Icon from './icon';
 import ProfileItems from './Profile';
-import MessageButton from './Chat/messageButton';
+import MessageButton from './Chat/MessageButton';
 import {ComponentInterface} from '../utils/registerComponent';
 import ProfileImage from './ImageProfile';
-import ChatList from './Chat/chatsList';
-import ChatUsersDropdown from './Dropdown/chatUsers';
+import ChatList from './Chat/ChatsList';
+import ChatUsersDropdown from './Dropdown/ChatUsers';
 import ChatListItem from './Chat/chatsListItem';
 import ChatMessages from './Chat/chatMessages';
 
