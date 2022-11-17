a8ffb1fb1d4466a2b9ba9fa964fa6d1e567060a6
Ekaterina Kovergina <ekaterina.kovergina@idp.zyfra.com>
2022-11-17 15:35:57 +0300

    правки по ревью

--- src/pages/Profile/Change/profileChangePage.ts
+++ src/pages/Profile/Change/profileChangePage.ts
@@ -8,16 +8,21 @@ import AuthController from '../../../controllers/AuthController';
 export class ProfileChangePageBase extends Components {
     constructor() {
         super({
-            onSubmit: () => {
-                const values = Object
-                    .values(this.children)
-                    .filter(child => child instanceof ProfileItems)
-                    .map((child) => ([(child as ProfileItems).getName(), (child as ProfileItems).getValue()]));
+            events: {
+                submit:(e: Event) => {
+                    e.preventDefault()
+                    console.log('lll')
+                    const values = Object
+                        .values(this.children)
+                        .filter(child => child instanceof ProfileItems)
+                        .map((child) => ([(child as ProfileItems).getName(), (child as ProfileItems).getValue()]))
+                    const data = Object.fromEntries(values);
+                    ProfileController.changeUserData(data);
+                    console.log(values)
+                    console.log(data)
+                },
+            }
 
-                const data = Object.fromEntries(values);
-
-                ProfileController.changeUserData(data);
-            },
         });
     }
 
